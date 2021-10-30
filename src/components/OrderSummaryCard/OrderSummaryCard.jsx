de4887644eb6ede6249fed89a473f175f79953fc
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-10-29 22:06:50 -0700

    Add initial mobile styles for hero and title. Add max card width for responsive growth.

--- src/components/OrderSummaryCard/OrderSummaryCard.jsx
+++ src/components/OrderSummaryCard/OrderSummaryCard.jsx
@@ -1,11 +1,12 @@
 import './OrderSummaryCard.scss';
 import PlanDetails from '../PlanDetails/PlanDetails';
 import Actions from '../Actions/Actions';
+import heroImg from '../../assets/images/illustration-hero.svg';
 
 const OrderSummaryCard = () => {
     return (
         <section className="order-summary">
-            <img className="order-summary__hero" src="" alt="dancing person"/>
+            <img className="order-summary__hero" src={heroImg} alt="dancing person"/>
             <h1 className="order-summary__title">Order Summary</h1>
             <p className="order-summary__description">
                 You can now listen to millions of songs, audiobooks, and podcasts on any 
