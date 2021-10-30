e89ac632d1501d26e2bbe5d5fc9ae4bdc3c706b7
Kayla Bonneteau <k.bonneteau@gmail.com>
2021-10-29 22:58:26 -0700

    Add initial content, styles and layout for mobile on PlanDetails component.

--- src/components/PlanDetails/PlanDetails.jsx
+++ src/components/PlanDetails/PlanDetails.jsx
@@ -1,10 +1,16 @@
 import './PlanDetails.scss';
+import musicIcon from '../../assets/images/icon-music.svg';
 
 const PlanDetails = () => {
     return (
-        <div>
-            
-        </div>
+        <article className="plan-details">
+            <img className="plan-details__plan-icon" src={musicIcon} alt="music note"/>
+            <div className="plan-details__detail-container">
+                <h2 className="plan-details__plan-name">Annual Plan</h2>
+                <p className="plan-details__plan-price">$59.99/year</p>
+            </div>
+            <button className="plan-details__change-plan">Change</button>
+        </article>
     );
 };
 
